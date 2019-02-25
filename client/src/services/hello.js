dcc5cba93d8bf5ef2e580c16d09356b602246596
stadibo <jesperleonard@gmail.com>
2019-02-25 22:02:29 +0200

    Restructure project and ready for deploy

--- client/src/services/hello.js
+++ client/src/services/hello.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3001/api'
+const url = '/api'
 
 const getApi = async () => {
   const response = await axios.get(`${url}/`)
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express')
-const app = express()
+const path = require('path');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 // const cron = require('node-cron')
 
+const app = express()
+
 // Middleware
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('/api/', (req, res) => {
   res.send('Hello from the API!')
 })
 
-const PORT = 3001
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
+
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
